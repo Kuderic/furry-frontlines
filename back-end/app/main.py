956ffb7c1025c61b28ab454bdcd417d6f1051b62
Eric McCarthy <ericm4@illinois.edu>
2024-07-05 16:53:44 -0500

    Added messaging between clients

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, WebSocketState
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
-app = FastAPI()
 
+app = FastAPI()
 
 # Mount the static files directory
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Initialize the templates directory
 templates = Jinja2Templates(directory="app/templates")
+
+# Variable to manage connected clients
+connected_clients = []
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -30,10 +33,16 @@ async def log_requests(request: Request, call_next):
 async def websocket_endpoint(websocket: WebSocket):
     client_ip = websocket.client.host
     await websocket.accept()
+    connected_clients.append(websocket)
+    print(f"{client_ip} has started a new websocket connection.\n List of connected clients:\
+          {connected_clients}")
     try:
         while True:
             data = await websocket.receive_text()
             print(f"Received message: {data}")
-            await websocket.send_text(f"We love gay furries!")
+            for client in connected_clients:
+                if client.application_state == WebSocketState.CONNECTED:
+                    await client.send_text(f"{client.client.host} says {data}")
     except WebSocketDisconnect:
         print(f"WebSocket connection closed by {client_ip}")
+        connected_clients.remove(websocket)
